7cb7accf5c31246100e02effab6c3ee9317a8b5e
靖东 林 <2807522347@qq.com>
2024-01-16 16:03:22 +0800

    创建后端项目

--- backend/src/main/java/com/kob/backend/pk/indexController.java
+++ backend/src/main/java/com/kob/backend/pk/indexController.java
@@ -1,7 +1,19 @@
-package com.kob.backend.pk;/**
+package com.kob.backend.pk;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
  * @Description TODO
  * @Date 2024/1/16 15:44
  * @Author Lin
  * @Version 1.0
- */public class indexController {
+ */
+@Controller
+@RequestMapping("/pk/")
+public class indexController {
+    @RequestMapping("index/")
+    public String index(){
+        return "/pk/index.html";
+    }
 }
