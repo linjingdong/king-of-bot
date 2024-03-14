a22002d2b1afbde6d3424f431dad5f36a50511af
potato0911 <2807522347@qq.com>
2024-03-14 15:54:17 +0800

    实现创建蛇+蛇的键盘监听，为实现移动

--- web/src/asssets/scripts/GameMap.js
+++ web/src/asssets/scripts/GameMap.js
@@ -1,4 +1,5 @@
 import { AcGameObect } from "./AcGameObject";
+import { Snake } from "./Snake";
 import { Wall } from "./Wall";
 
 export class GameMap extends AcGameObect {
@@ -14,10 +15,15 @@ export class GameMap extends AcGameObect {
         this.L = 0; // 定义绝对距离，默认每个单位的距离大小
 
         this.rows = 13; // 行数
-        this.cols = 13; // 列数
+        this.cols = 14; // 列数
 
         this.walls = []; // 墙
         this.inner_walls_count = 20; //障碍物
+
+        this.snakes = [
+            new Snake({ id: 0, color: "#f94848", r: this.rows - 2, c: 1 }, this),
+            new Snake({ id: 1, color: "#4876ec", r: 1, c: this.cols - 2 }, this),
+        ];
     }
 
     check_connectivity(g, sx, sy, tx, ty) {
@@ -61,10 +67,10 @@ export class GameMap extends AcGameObect {
             for (let j = 0; j < 1000; j++) {
                 let r = parseInt(Math.random() * this.rows);
                 let c = parseInt(Math.random() * this.cols);
-                if (g[r][c] || g[c][r]) continue;
+                if (g[r][c] || g[this.rows - 1 - r][this.cols - 1 - c]) continue; // 中心对称，实现公平竞技
                 if (r == this.rows - 2 && c == 1 || r == 1 && c == this.cols - 2) continue;
 
-                g[r][c] = g[c][r] = true;
+                g[r][c] = g[this.rows - 1 - r][this.cols - 1 - c] = true;
                 break;
             }
         }
@@ -80,14 +86,32 @@ export class GameMap extends AcGameObect {
                 }
             }
         }
-
         return true;
+    }
+
+    add_listening_events() {
+        this.ctx.canvas.focus();
+
+        const [snake0, snake1] = this.snakes;
+
+        this.ctx.canvas.addEventListener("keydown", e => {
+            if (e.key === 'w') snake0.set_direction(0);
+            else if (e.key === 'd') snake0.set_direction(1);
+            else if (e.key === 's') snake0.set_direction(2);
+            else if (e.key === 'a') snake0.set_direction(3);
+            else if (e.key === 'ArrowUp') snake1.set_direction(0);
+            else if (e.key === 'ArrowRight') snake1.set_direction(1);
+            else if (e.key === 'ArrowDown') snake1.set_direction(2);
+            else if (e.key === 'ArrowLeft') snake1.set_direction(3);
+        });
     }
 
     start() {
         for (let i = 0; i < 1000; i++) {
             if (this.create_walls()) break;
         }
+
+        this.add_listening_events();
     }
 
     // 更新时每个单位的大小每一帧刷新一遍
@@ -100,8 +124,25 @@ export class GameMap extends AcGameObect {
         this.ctx.canvas.height = this.L * this.rows;
     }
 
+    check_ready() { // 判断两条蛇是否准备好下一回合了
+        for (const snake of this.snakes) {
+            if (snake.status !== "idle") return false;
+            if (snake.direction === -1) return false;
+        }
+        return true;
+    }
+
+    next_step() {
+        for (const snake of this.snakes) {
+            snake.next_step();
+        }
+    }
+
     update() {
         this.update_size();
+        if (this.check_ready()) {
+            this.next_step();
+        }
         this.render();
     }
 
